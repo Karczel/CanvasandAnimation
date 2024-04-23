3f5e36f73e0d95b449b1f75955c54946a3b7ff42
Karczel <92095164+Karczel@users.noreply.github.com>
2024-04-23 11:03:57 +0700

    a

---
+++ Loading Screens/Example_1.py
@@ -0,0 +1,3 @@
+# From :https://www.youtube.com/watch?v=A9mxZGV_zmI
+
+# take away multi thread part
---
+++ Loading Screens/Mine_1.py
@@ -0,0 +1,4 @@
+# with other task running in the background
+# loop until other thread is finished
+# show as much progress as actual progress
+
